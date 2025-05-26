f911145c5104628b68c0f6a540c8bd7fa4405f18
jcasanella <jordikiter@hotmail.es>
2025-05-27 00:08:04 +0100

    Fix deserialization process

--- src/main/java/org/jordi/client/TickerService.java
+++ src/main/java/org/jordi/client/TickerService.java
@@ -1,12 +1,14 @@
 package org.jordi.client;
 
 import jakarta.ws.rs.*;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jordi.client.utils.JsonContentTypeFilter;
 import org.jordi.model.CryptoResponse;
 
 @Path("/ticker")
-@Produces("application/json")
-@Consumes({"text/json"})
+@Produces({"application/json", "text/json"})
+@RegisterProvider(JsonContentTypeFilter.class)
 @RegisterRestClient
 public interface TickerService {
     @GET
